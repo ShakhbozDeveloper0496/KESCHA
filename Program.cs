5414513ce50d7a21e9397d2c9194bfc3f4d06aff
Shaxboz <yangiyev1996@gmail.com>
2024-02-12 22:14:08 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+//string turlarini ko'rib chiqamiz
+
+
+
+
+// Console.WriteLine(sizeof(short));
+// Console.WriteLine(sizeof(int));
+// Console.WriteLine(sizeof(long));
+ 
+// Console.WriteLine(short.MinValue); 
+// Console.WriteLine(short.MaxValue);
+
+// Console.WriteLine(int.MinValue);
+// Console.WriteLine(int.MaxValue);
+
+// Console.WriteLine(long.MinValue);
+// Console.WriteLine(long.MaxValue);
+
+
+
+// Console.WriteLine($"Size of short: {sizeof(short)} bayt"); //interpolation string
+// Console.WriteLine($"size of int: {sizeof(int)} bayt");
+// Console.WriteLine($"size of long: {sizeof(long)} bayt");
+
+// Console.WriteLine( $"qancha raqamgacha qabul qiladi: {ushort.MinValue}");
+// Console.WriteLine( $"qancha raqamgacha qabul qiladi: {ushort.MaxValue}");
+
+// Console.WriteLine($"qancha raqamgacha qabul qiladi: {uint.MinValue}");
+// Console.WriteLine($"qancha raqamgacha qabul qiladi: {uint.MaxValue}");
+
+// Console.WriteLine($"qancha raqamgacha qabul qiladi: {ulong.MinValue}");
+// Console.WriteLine($"qancha raqamgacha qabul qiladi: {ulong.MaxValue}");
+
+
+
+// int a = 3232;
+// short b = (short)a;
+// System.Console.WriteLine(b);
+
+
+
+System.Console.WriteLine("Hello ! \nCan you enter your name: ");
+string name= Console.ReadLine();
+System.Console.WriteLine($"Hi, Your name is {name}");
+
+System.Console.Write("Please enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"your age is {age}");
+
+
+
+
